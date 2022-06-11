fa3ccfdf9a6e7e1d2daf4ccfb3ea970548310639
Zapalka <93160769+PiotrZapala@users.noreply.github.com>
2022-06-12 01:14:21 +0200

    Update tests.py

--- genetic test/tests.py
+++ genetic test/tests.py
@@ -3,7 +3,6 @@ import time
 import numpy as np
 import tsp_initialize as init
 import tsp_functions as func
-import concurrent.futures
 from genetic import individual  
 from genetic import selection 
 from genetic import selection_population 
@@ -61,8 +60,6 @@ class genetic_algorithm:
         self.size_of_population = size_of_population
         self.iterations = iterations
         self.best_known = best_known
-        #self.type_of_stop_condition = type_of_stop_condition
-        #self.number_for_stop = number_for_stop
         self.parents = parents
         self.best_solutions = best_solutions
         self.distance_matrix = distance_matrix
@@ -73,10 +70,9 @@ class genetic_algorithm:
 
 
     def algorithm(self):
-        #stop = stop_condition(self.type_of_stop_condition)
         i = 0
         self.current_best.append(self.best_solutions[0])
-        while i < 4000: #self.global_iterations_without_update
+        while i < 4000: 
             sel = selection(self.type_of_selection, self.parents)
             fathers = sel.selected_fathers
             mothers = sel.selected_mothers
@@ -103,68 +99,43 @@ class genetic_algorithm:
                 self.current_best.append(self.best_solutions[-1])
             mut = mutation(self.distance_matrix, self.size_of_population, self.probability_of_mutation, self.type_of_mutation, self.parents)
             self.parents = mut.set_of_individuals
-            #stop.update_stop_condition()
             i += 1
         return self.best_solutions, self.parents
 
 def test():
     size_of_population = [40, 80, 120, 160, 200, 240]
-    probability_of_mutation = [5, 10, 15, 20]
-    number_of_iterations_without_update = [50, 100, 150, 200]
+    probability_of_mutation = [5, 10, 15, 20, 25]
+    number_of_iterations_without_update = [50, 100, 150, 200, 250]
     best_known_solution = 7544
     file_name = 'berlin52.tsp'
-    type_of_selection1 = 'roulette'
-    type_of_selection2 = 'roulette' 
+    type_of_selection1 = 'roulette' 
     type_of_selection_population1 = 'roulette'
-    type_of_selection_population2 = 'roulette'
     type_of_crossing1 = 'PMX'
-    type_of_crossing2 = 'PMX'
     type_of_mutation1 = 'swap'
-    type_of_mutation2 = 'swap'
     for it in range(len(size_of_population)):
         file = open('tests/'+file_name+'-size'+str(size_of_population[it])+'.txt', 'w')
         file.write('1.(size of population)' + ',' + '2.(probability of mutation)' + ',' + '3.(number of iterations without update)' + ',' + '4.(time)' + ',' + '5.(best solution)' + ',' + '6.(prd)' + '\n')
         size_of_population1 = size_of_population[it]
-        size_of_population2 = size_of_population[it]
         pop1 = population(size_of_population1, file_name)
-        pop2 = population(size_of_population1, file_name)
         best_known = best_known_solution
         parents1 = pop1.set_of_individuals
-        parents2 = pop2.set_of_individuals
         best_solutions1 = pop1.best_solution
-        best_solutions2 = pop2.best_solution
         size_of_population1 = pop1.size_of_population
-        size_of_population2 = pop2.size_of_population
         distance_matrix1 = pop1.distance_matrix
-        distance_matrix2 = pop2.distance_matrix
         for jt in range(len(probability_of_mutation)):
             probability_of_mutation1 = probability_of_mutation[jt]
-            probability_of_mutation2 = probability_of_mutation[jt]
             for ka in range(len(number_of_iterations_without_update)):
                 number_of_iterations_without_update1 = number_of_iterations_without_update[ka]
-                number_of_iterations_without_update2 = number_of_iterations_without_update[ka]
                 best_individual1 = []
-                best_individual2 = []
-                minn = []
-                with concurrent.futures.ThreadPoolExecutor() as executor:
-                    start = time.time()         
-                    gen1 = executor.submit(genetic_algorithm, str(type_of_selection1), str(type_of_selection_population1), str(type_of_crossing1), str(type_of_mutation1), probability_of_mutation1, size_of_population1, number_of_iterations_without_update1, best_known, parents1, best_solutions1, distance_matrix1)
-                    gen2 = executor.submit(genetic_algorithm, str(type_of_selection2), str(type_of_selection_population2), str(type_of_crossing2), str(type_of_mutation2), probability_of_mutation2, size_of_population2, number_of_iterations_without_update2, best_known, parents2, best_solutions2, distance_matrix2)
-                    my_time = end - start
-                    end = time.time()
-                    results1 = gen1.result()
-                    results2 = gen2.result()
-                length1 = len(results1.best_solutions)
-                length2 = len(results2.best_solutions)
-                for j in range(length1):
-                    best_individual1.append(results1.best_solutions[j].phenotype)
-                for k in range(length2):
-                    best_individual2.append(results2.best_solutions[k].phenotype)
-                minn.append(min(best_individual1))
-                minn.append(min(best_individual2))
-                beesstt = min(minn)
-                prd = 100 * ((beesstt - best_known) / best_known)
-                file.write(str(size_of_population[it]) + ',' + str(probability_of_mutation[jt]) + ',' + str(number_of_iterations_without_update[ka]) + ',' + str(my_time) + ',' + str(beesstt) +  ',' + str(prd) + '\n')
+                start = time.time()         
+                best_solutions, parents = genetic_algorithm(str(type_of_selection1), str(type_of_selection_population1), str(type_of_crossing1), str(type_of_mutation1), probability_of_mutation1, size_of_population1, number_of_iterations_without_update1, best_known, parents1, best_solutions1, distance_matrix1)
+                end = time.time()
+                my_time = end - start
+                for j in range(len(best_solutions)):
+                    best_individual1.append(best_solutions[j].phenotype)
+                minn = min(best_individual1)
+                prd = 100 * ((minn - best_known) / best_known)
+                file.write(str(size_of_population[it]) + ',' + str(probability_of_mutation[jt]) + ',' + str(number_of_iterations_without_update[ka]) + ',' + str(my_time) + ',' + str(minn) +  ',' + str(prd) + '\n')
 
 if __name__ == '__main__':
     test()
